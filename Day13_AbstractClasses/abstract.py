f56903af220fb7d7ef3641cedba5b65f5bcac690
Danilo Aleixo <danilo_aleixo@hotmail.com>
2017-03-31 20:39:18 -0300

    dia 13

--- Day13_AbstractClasses/abstract.py
+++ Day13_AbstractClasses/abstract.py
@@ -42,3 +42,33 @@
 # Title: The Alchemist
 # Author: Paulo Coelho
 # Price: 248
+
+from abc import ABCMeta, abstractmethod
+class Book:
+    __metaclass__ = ABCMeta
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author   
+    @abstractmethod
+    def display(): pass
+
+
+#Write MyBook class
+class MyBook(Book):
+	def __init__(self, title, author, price):
+		self.title=title
+		self.author=author
+		self.price=price
+
+	def display(self):
+		print "Title: " + self.title
+		print "Author: " + self.author
+		print "Price: " + str(self.price)
+
+
+
+title=raw_input()
+author=raw_input()
+price=int(raw_input())
+new_novel=MyBook(title,author,price)
+new_novel.display()
